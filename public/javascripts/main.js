d9bd041e6643f2e62310495384ee2fc9bcddbc65
Jeffrey <jeff.isabella@gmail.com>
2023-05-21 21:12:04 -0400

    settings markup/css
    settings markup/css

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -497,7 +497,44 @@ function chipsInnerMarkup(integerValue) {
   return markup;
 }
 
+// const allRanges = document.querySelectorAll(".range-wrap");
+// allRanges.forEach(wrap => {
+//   const range = wrap.querySelector(".range");
+//   const bubble = wrap.querySelector(".bubble");
+
+//   range.addEventListener("input", () => {
+//     setBubble(range, bubble);
+//   });
+//   setBubble(range, bubble);
+// });
+
+function setBubble(range, bubble) {
+  const val = range.value;
+  const min = range.min ? range.min : 0;
+  const max = range.max ? range.max : 100;
+  const newVal = Number(((val - min) * 100) / (max - min));
+  bubble.innerHTML = val;
+
+  // Sorta magic numbers based on size of the native UI thumb
+  // bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
+  // bubble.style.left = `calc(${newVal}%`;
+  // bubble.style.left = `calc(${newVal}`;
+  // bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
+}
+
 $(document).ready(function() {
+  
+  const allRanges = document.querySelectorAll(".range-wrap");
+  allRanges.forEach(wrap => {
+    const range = wrap.querySelector(".range");
+    const bubble = wrap.querySelector(".bubble");
+
+    range.addEventListener("input", () => {
+      setBubble(range, bubble);
+    });
+    setBubble(range, bubble);
+  });
+  
   $("#make-smaller").click(function (e) {
     change_card_size("player", "decrease");
   });
